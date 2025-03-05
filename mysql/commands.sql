65ad461fabf7ba13229bdce298729a6bfbca5417
Evan Riddick <riddickevan841@gmail.com>
2025-03-05 09:17:54 -0500

    Update commands.sql

--- mysql/commands.sql
+++ mysql/commands.sql
@@ -3,3 +3,14 @@ VALUES
   (Jonathon Myers, 860-458-9678, jmyers@outlook.com),
   (),
   ();
+
+INSERT INTO courts
+VALUES
+  (1, 0),
+  (2, 0),
+  (3, 0),
+  (4, 0),
+  (5, 0),
+  (6, 0),
+  (7, 0),
+  (8, 0);
